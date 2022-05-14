fd2891f45a335ad61e93bccacdf56cc738e965e4
AXN4 <99322970+AXN4@users.noreply.github.com>
2022-05-14 14:47:11 +0530

    ☺️💖

--- plugins/gitv2.js
+++ plugins/gitv2.js
@@ -11,7 +11,7 @@ Alexa.addCommand({pattern: 'owner', fromMe: false, desc: Lang.NUMBER}, (async (m
             const vcard = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n' 
             + 'FN:' + Config.OWNER + '\n' //created afnanplk, please copy this with credit..
-            + 'ORG:Alexa;\n' 
+            + 'ORG:RDX;\n' 
             + 'TEL;type=CELL;type=VOICE;waid=' + Config.PHONE + ':' + Config.PHONE + ' \n'
             + 'END:VCARD'
 await message.client.sendMessage(message.jid, {displayname: "〽️ʙᴏᴛ ᴏᴡɴᴇʀ", vcard: vcard}, MessageType.contact, { mimetype: Mimetype.contact, quoted: message.data, ptt: true,quoted: { key: { participant : '0@s.whatsapp.net'},message: {orderMessage: {itemCount : 777777,status: 1,surface : 1,message: Config.SKV,orderTitle: `THIS IS NEW?`,thumbnail: img, sellerJid: Config.JID }}}});
