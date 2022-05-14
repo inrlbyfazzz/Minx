1088155039e58b5104f6e5cf5c5b79dd82cf6896
AXN4 <99322970+AXN4@users.noreply.github.com>
2022-05-14 14:54:09 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,4 +1,4 @@
-const Abu = require('../events');
+const Minx = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 const Config = require('../config');
